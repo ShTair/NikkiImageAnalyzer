4d4f08cffc2fad9426fca4fae3f6c7dadc492812
ShTair <a@vsh.jp>
2017-04-18 18:50:03 +0900

    画像を生成する

--- NikkiImageAnalyzer/MainWindow.xaml.cs
+++ NikkiImageAnalyzer/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +30,29 @@ namespace NikkiImageAnalyzer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            using (var bmp = new Bitmap(target))
+            {
+                using (var sp = new Bitmap(50, 1280))
+                {
+                    using (var g = Graphics.FromImage(sp))
+                    {
+                        DrowImage(g, bmp, )
+                    }
 
+                }
+            }
+
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                string plainText = api.GetTextFromImage();
+                Console.WriteLine(plainText);
+            }
+        }
+
+        private void DrowImage(Graphics dst, System.Drawing.Image src, int x1, int y1, int x2, int y2, int w, int h)
+        {
+            dst.DrawImage(src, new System.Drawing.Rectangle(x1, x2, w, h), new System.Drawing.Rectangle(x1, x2, w, h), GraphicsUnit.Pixel);
         }
     }
 }
