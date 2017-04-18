ecebd235e5659b5b4ea10f33865930b51b63b71a
ShTair <a@vsh.jp>
2017-04-18 13:06:17 +0900

    ビデオを指定するBOX

--- NikkiImageAnalyzer/MainWindow.xaml.cs
+++ NikkiImageAnalyzer/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace NikkiImageAnalyzer
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
